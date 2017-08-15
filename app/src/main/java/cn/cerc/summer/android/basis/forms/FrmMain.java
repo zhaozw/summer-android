744d14df51c2e87238a7b87febdc7e58b71e0692
xusonghua <13268229995@163.com>
2017-08-15 14:54:51 +0800

    取得网上视频文件并进行播放—未完成

--- app/src/main/java/cn/cerc/summer/android/basis/forms/FrmMain.java
+++ app/src/main/java/cn/cerc/summer/android/basis/forms/FrmMain.java
@@ -47,17 +47,18 @@ import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 
-import cn.cerc.summer.android.basis.core.WebConfig;
+import cn.cerc.summer.android.basis.core.Constans;
 import cn.cerc.summer.android.basis.core.MainPopupMenu;
 import cn.cerc.summer.android.basis.core.MyApp;
 import cn.cerc.summer.android.basis.core.MyBroadcastReceiver;
-import cn.cerc.summer.android.basis.core.Constans;
 import cn.cerc.summer.android.basis.core.PermissionUtils;
 import cn.cerc.summer.android.basis.core.ScreenUtils;
-import cn.cerc.summer.android.basis.view.DragPointView;
+import cn.cerc.summer.android.basis.core.WebConfig;
 import cn.cerc.summer.android.basis.view.BrowserView;
+import cn.cerc.summer.android.basis.view.DragPointView;
 import cn.cerc.summer.android.basis.view.ShowDialog;
 import cn.cerc.summer.android.basis.view.ShowPopupWindow;
+import cn.cerc.summer.android.parts.movie.FrmPlayMovie;
 import cn.jpush.android.api.JPushInterface;
 import cn.jpush.android.api.TagAliasCallback;
 
@@ -408,7 +409,8 @@ public class FrmMain extends AppCompatActivity implements View.OnLongClickListen
                         browser.loadUrl(homeUrl);
                         break;
                     case 3:
-                        startActivityForResult(new Intent(FrmMain.this, FrmSettings.class).putExtra("address", browser.getUrl()), REQUEST_SETTING);
+                       // startActivityForResult(new Intent(FrmMain.this, FrmSettings.class).putExtra("address", browser.getUrl()), REQUEST_SETTING);
+                        startActivity(new Intent(FrmMain.this, FrmPlayMovie.class));
                         break;
                     case 4:
                         clearCacheFolder(FrmMain.this.getCacheDir(), System.currentTimeMillis());
@@ -424,7 +426,7 @@ public class FrmMain extends AppCompatActivity implements View.OnLongClickListen
                             browser.reload();
                         break;
                     case 6:
-                        browser.reload();
+                         browser.reload();
                         break;
                 }
                 popupWindow.dismiss();
--- app/src/main/java/cn/cerc/summer/android/parts/movie/FrmPlayMovie.java
+++ app/src/main/java/cn/cerc/summer/android/parts/movie/FrmPlayMovie.java
@@ -2,19 +2,55 @@ package cn.cerc.summer.android.parts.movie;
 
 import android.content.Context;
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
+import android.media.MediaPlayer;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.MediaController;
+import android.widget.Toast;
+import android.widget.VideoView;
 
 import com.mimrc.vine.R;
 
 import cn.cerc.summer.android.parts.image.FrmZoomImage;
 
 public class FrmPlayMovie extends AppCompatActivity {
+    private VideoView videoView;//视频播放
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_frm_play_movie);
+
+        //本地的视频 需要在手机SD卡根目录添加一个 fl1234.mp4 视频
+        String videoUrl1 = Environment.getExternalStorageDirectory().getPath() + "/f1234.mp4";
+
+        //网络视频
+        String videoUrl2 = Utils.videoUrl;
+
+        Uri uri = Uri.parse( videoUrl2 );
+        videoView = (VideoView)this.findViewById(R.id.videoView );
+
+        //设置视频控制器
+        videoView.setMediaController(new MediaController(this));
+
+        //播放完成回调
+        videoView.setOnCompletionListener( new MyPlayerOnCompletionListener());
+
+        //设置视频路径
+        videoView.setVideoURI(uri);
+
+        //开始播放视频
+        videoView.start();
+    }
+    class MyPlayerOnCompletionListener implements MediaPlayer.OnCompletionListener {
+
+        @Override
+        public void onCompletion(MediaPlayer mp) {
+            Toast.makeText( FrmPlayMovie.this, "播放完成了", Toast.LENGTH_SHORT).show();
+        }
     }
 
     public static void startForm(Context context, String urlMovie) {
@@ -23,4 +59,11 @@ public class FrmPlayMovie extends AppCompatActivity {
         intent.putExtra("url", urlMovie);
         context.startActivity(intent);
     }
-}
+    public class Utils {
+
+        public static final String videoUrl = "http://clips.vorwaerts-gmbh.de/big_buck_bunny.mp4" ;
+    }
+    }
+
+
+
